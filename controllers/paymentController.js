b22c82160f9d07c404d2b42de738ad7361061f79
sairugved <sairugved80@gmail.com>
2025-02-28 13:27:54 +0530

    error removed

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,6 @@
 const Team = require('../models/team');
 const Payment = require('../models/payment');
-const { generatePDF } = require('../utils/pdfGenerator');
+const { generatePDF } = require('../utils/pdfgenerator');
 const cloudinary = require('../config/cloudinary');
 const streamifier = require('streamifier');
 
